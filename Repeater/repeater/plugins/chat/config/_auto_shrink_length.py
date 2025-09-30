79b925af46ed135eceb0d82113ec16d72c55873b
Qeggs <get_boot@outlook.com>
2025-10-01 03:57:33 +0800

    feat(repeater): 添加设置自动收缩长度的命令功能
    新增 `set_auto_shrink_length` 指令，支持通过聊天指令动态设置 `auto_shrink_length` 配置项。
    该指令支持多个别名，包括 `sasl`、`setTemperature` 等，提升用户交互灵活性。
    指令执行时会校验输入是否为整数，并反馈设置结果或错误信息。

---
+++ Repeater/repeater/plugins/chat/config/_auto_shrink_length.py
@@ -0,0 +1,35 @@
+from nonebot import on_command
+from nonebot.rule import to_me
+from nonebot.params import CommandArg
+from nonebot.adapters import Message
+from nonebot.adapters.onebot.v11 import MessageEvent, MessageSegment
+from nonebot.adapters import Bot
+
+from ._core import ChatCore, RepeaterDebugMode
+from ..assist import StrangerInfo
+
+set_auto_shrink_length = on_command('setTemperature', aliases={'sasl', 'set_auto_shrink_length', 'Set_Auto_Shrink_Length', 'SetAutoShrinkLength'}, rule=to_me(), block=True)
+
+@set_auto_shrink_length.handle()
+async def handle_set_auto_shrink_length(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
+    stranger_info = StrangerInfo(bot=bot, event=event, args=args)
+
+    msg = stranger_info.message_str.strip()
+    reply = MessageSegment.reply(event.message_id)
+
+    try:
+        auto_shrink_length = int(msg)
+    except ValueError:
+        await set_auto_shrink_length.finish(
+            reply +
+            '====Chat.Set_Auto_Shrink_Length====\n> 设置错误，请输入一个整数'
+        )
+
+
+    chat_core = ChatCore(stranger_info.name_space.namespace)
+    if RepeaterDebugMode:
+        await set_auto_shrink_length.finish(reply + f'[Chat.Set_Auto_Shrink_Length|{chat_core.name_space}|{stranger_info.nickname}]:{msg}')
+    else:
+        code, text = await chat_core.set_config("auto_shrink_length", auto_shrink_length)
+
+        await set_auto_shrink_length.finish(reply + f'====Chat.Set_Auto_Shrink_Length====\n> {chat_core.name_space}\nHTTP Code: {code}\n\nTemperature: {auto_shrink_length}')
