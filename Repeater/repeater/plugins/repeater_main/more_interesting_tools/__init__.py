c84d33102645678ae2400819188c08f71764161c
Qeggs <get_boot@outlook.com>
2025-10-02 21:51:27 +0800

    refactor(repeater): 重命名插件目录结构
    将 `Repeater/repeater/plugins/chat` 目录重命名为 `Repeater/repeater/plugins/repeater_main`

--- Repeater/repeater/plugins/repeater_main/more_interesting_tools/__init__.py
+++ Repeater/repeater/plugins/repeater_main/more_interesting_tools/__init__.py
@@ -1,7 +1,9 @@
 from ._choose_group_member import choose_group_member_handle
 from ._recent_speaking_ranking import recent_speaking_ranking_handle
+from ._audio_to_file import audio_to_file_handle
 
 __all__ = [
+    "audio_to_file_handle",
     "choose_group_member_handle",
     "recent_speaking_ranking_handle",
 ]
