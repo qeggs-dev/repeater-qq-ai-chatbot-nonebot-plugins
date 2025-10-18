36d44fe0f3a5e78758b061206bbabc67bec9aa84
Qeggs <get_boot@outlook.com>
2025-10-18 11:46:50 +0800

    refactor(user_file): 重构用户数据文件发送模块
    将 `userFile` 目录重命名为 `user_file`，以符合命名规范。
    更新模块导入路径，并替换旧的核心类 `ChatCore` 为 `UserFileCore`。
    统一使用 `SendMsg` 类处理消息发送逻辑，优化调试模式下的输出方式。
    修改发送的文件名格式，包含命名空间前缀以增强辨识度。

---
+++ Repeater/repeater/plugins/repeater_main/command/user_file/send_session_file.py
@@ -0,0 +1,25 @@
+from nonebot import on_command
+from nonebot.rule import to_me
+from nonebot.params import CommandArg
+from nonebot.adapters import Message
+from nonebot.adapters.onebot.v11 import MessageEvent, MessageSegment
+from nonebot.adapters import Bot
+
+from .._clients import UserFileCore
+from ...assist import StrangerInfo, FileSender, SendMsg
+
+send_user_data_file = on_command('sendUserDataFile', aliases={'sudf', 'send_user_data_file', 'Send_User_Data_File', 'SendUserDataFile'}, rule=to_me(), block=True)
+
+@send_user_data_file.handle()
+async def handle_send_user_data_file(bot: Bot, event: MessageEvent):
+    stranger_info = StrangerInfo(bot=bot, event=event)
+    sendmsg = SendMsg("UserFile.Send_User_Data_File", send_user_data_file, stranger_info)
+
+    chat_core = UserFileCore(stranger_info.namespace_str)
+    sfurl = await chat_core.get_user_data_file_url()
+    if sendmsg.is_debug_mode:
+        await sendmsg.send_debug_mode()
+    else:
+        file_sender = FileSender(stranger_info)
+
+        await file_sender.send_file(sfurl, f"{stranger_info.namespace_str}_UserDataFile.zip")
