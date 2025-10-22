b3273804da2992cf8b30b00a54a2763f035e22d3
Qeggs <get_boot@outlook.com>
2025-10-22 11:04:43 +0800

    feat(ContextCore): 添加日志记录功能以增强调试能力
    为 ContextCore 类中的多个方法添加了详细的日志记录，包括 inject_context、withdraw、
    change_context_branch、delete_context 和 get_context_total_length 方法。同时引入了
    logger 模块并进行了绑定，以便更好地追踪上下文操作的执行流程。

--- Repeater/repeater/plugins/repeater_main/command/_clients/_context_core/_core.py
+++ Repeater/repeater/plugins/repeater_main/command/_clients/_context_core/_core.py
@@ -7,10 +7,13 @@ from typing import (
 
 from ....core_net_configs import *
 from ....assist import StrangerInfo, Response
+from ....logger import logger as base_logger
 from ._response import (
     WithdrawResponse,
     ContextTotalLengthResponse
 )
+
+logger = base_logger.bind(module = "Context.Core")
 
 class ContextCore:
     _httpx_client = httpx.AsyncClient()
@@ -20,6 +23,7 @@ class ContextCore:
     
     # region inject context
     async def inject_context(self, text: str, role: str) -> Response[None]:
+        logger.info("Injecting {role} context", role = role)
         response = await self._httpx_client.post(
             f'{INJECT_CONTEXT_ROUTE}/{self._info.namespace_str}',
             data={
@@ -36,6 +40,7 @@ class ContextCore:
     
     # region withdraw
     async def withdraw(self) -> Response[WithdrawResponse | None]:
+        logger.info("Withdrawing context")
         response = await self._httpx_client.post(
             f'{WIHTDRAW_CONTEXT_ROUTE}/{self._info.namespace_str}'
         )
@@ -49,6 +54,7 @@ class ContextCore:
     # endregion
     # region change subsession    
     async def change_context_branch(self, new_branch_id: str) -> Response[None]:
+        logger.info("Changing context branch to {new_branch_id}", new_branch_id = new_branch_id)
         response = await self._httpx_client.put(
             f'{CHANGE_CONTEXT_BRANCH_ROUTE}/{self._info.namespace_str}',
             data={
@@ -63,17 +69,8 @@ class ContextCore:
     # endregion
     
     # region delete
-    async def delete_session(self) -> Response[None]:
-        response = await self._httpx_client.delete(
-            f'{DELETE_CONTEXT_ROUTE}/{self._info.namespace_str}'
-        )
-        return Response(
-            code = response.status_code,
-            text = response.text,
-            data = None
-        )
-    
     async def delete_context(self) -> Response[None]:
+        logger.info("Deleting context")
         response = await self._httpx_client.delete(
             f'{DELETE_CONTEXT_ROUTE}/{self._info.namespace_str}'
         )
@@ -85,6 +82,7 @@ class ContextCore:
     # endregion
     
     async def get_context_total_length(self) -> Response[ContextTotalLengthResponse | None]:
+        logger.info("Getting context total length")
         response = await self._httpx_client.get(
             f'{GET_CONTEXT_LENGTH_ROUTE}/{self._info.namespace_str}'
         )
