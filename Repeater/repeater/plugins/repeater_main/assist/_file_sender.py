e6af1c71bceca58d36462d95048fd9cc1e2467b7
Qeggs <get_boot@outlook.com>
2025-11-20 17:52:18 +0800

    fix(repeater): 修复了FileSender不执行的问题
    - 修复了后面的source判断，使用了MessageSource而非字符串

--- Repeater/repeater/plugins/repeater_main/assist/_file_sender.py
+++ Repeater/repeater/plugins/repeater_main/assist/_file_sender.py
@@ -1,11 +1,11 @@
-from ._stranger_info import StrangerInfo
+from ._stranger_info import StrangerInfo, MessageSource
 
 class FileSender:
     def __init__(self, stranger_info: StrangerInfo):
         self.stranger_info = stranger_info
 
     async def send_file(self, url: str, file_name: str):
-        if self.stranger_info.source == "group":
+        if self.stranger_info.source == MessageSource.GROUP:
             data = {
                 "group_id": self.stranger_info._group_id,
                 "file": url,
@@ -13,7 +13,7 @@ class FileSender:
                 "folder_id": None
             }
             await self.stranger_info.bot.upload_group_file(**data)
-        elif self.stranger_info.source == "private":
+        elif self.stranger_info.source == MessageSource.PRIVATE:
             data = {
                 "user_id": self.stranger_info.user_id,
                 "file": url,
