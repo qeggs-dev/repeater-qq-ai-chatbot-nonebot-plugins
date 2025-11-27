298a0a0c6834e6e733f3a3a1e3879aab990f41ca
Qeggs <get_boot@outlook.com>
2025-11-27 22:57:27 +0800

    feat(repeater): 添加模块导出声明
    为 user_file 和 variable_expansion 模块添加 __all__ 列表,
    明确指定模块的公共接口, 提高代码可维护性和模块化程度。

--- Repeater/repeater/plugins/repeater_main/command/user_file/__init__.py
+++ Repeater/repeater/plugins/repeater_main/command/user_file/__init__.py
@@ -1,1 +1,5 @@
 from ._send_session_file import handle_send_user_data_file
+
+__all__ = [
+    "handle_send_user_data_file"
+]
--- Repeater/repeater/plugins/repeater_main/command/variable_expansion/__init__.py
+++ Repeater/repeater/plugins/repeater_main/command/variable_expansion/__init__.py
@@ -1,3 +1,9 @@
 from ._var_expand import handle_var_expand
 from ._var_expand_text import handle_var_expand_text
 from ._var_expand_image import handle_var_expand_image
+
+__all__ = [
+    "handle_var_expand",
+    "handle_var_expand_text",
+    "handle_var_expand_image",
+]
